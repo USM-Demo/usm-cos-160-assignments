eefa3ecb0f19a4678921a960fd821b8f1c8c5ce7
ahaas207 <86922705+ahaas207@users.noreply.github.com>
2023-02-07 09:43:39 -0500

    Update face.java

--- face.java
+++ face.java
@@ -1,18 +1,16 @@
 /*
- Name: Aimee Haas
- COS 160 Spring 2021 Prof. MacLeod, Mon/Wed 12:30
+ Name: Student Name
+ COS 160 Spring XXXX Prof. XXXXX, Mon/Wed 12:30
  Program #1
  File Name: Face.java
  */
 public class face {
 
     public static void main(String[] args) {
+     
+            // Complete the face
             System.out.println("   \" \" \" \" \" \" \" ");
-            System.out.println("   |    _  _    | ");
-            System.out.println("0 |     o o    | 0");
-            System.out.println("   |       7     |");
-            System.out.println("    \\   ___   /");
-            System.out.println("      \\____/");
+     
 
     }
 
