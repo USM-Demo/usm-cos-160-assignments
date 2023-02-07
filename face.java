643413096eff35ba25d2c6b65dc6ae70679bac42
ahaas207 <86922705+ahaas207@users.noreply.github.com>
2023-02-07 09:38:54 -0500

    Example Assignment 1 Submission

---
+++ face.java
@@ -0,0 +1,19 @@
+/*
+ Name: Aimee Haas
+ COS 160 Spring 2021 Prof. MacLeod, Mon/Wed 12:30
+ Program #1
+ File Name: Face.java
+ */
+public class face {
+
+    public static void main(String[] args) {
+            System.out.println("   \" \" \" \" \" \" \" ");
+            System.out.println("   |    _  _    | ");
+            System.out.println("0 |     o o    | 0");
+            System.out.println("   |       7     |");
+            System.out.println("    \\   ___   /");
+            System.out.println("      \\____/");
+
+    }
+
+}
